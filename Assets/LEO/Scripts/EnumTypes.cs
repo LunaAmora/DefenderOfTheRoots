c9386bdfd5ef292bea0baef87cdcd34ba0d300d8
SrLmo <srlmo@hotmail.com.br>
2023-02-02 07:24:37 -0300

    Add base drag and snap

---
+++ Assets/LEO/Scripts/EnumTypes.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumTypes : MonoBehaviour { }
+
+
+public enum DraggableType
+{
+    Resource, Turret
+}
+
+public enum TurretType
+{
+    Collector, Machinegun, Sniper, Shotgun
+}
+
+public enum DragState
+{
+    Nulo, Dragging, Snaped, Inside
+}
+
+
+
+
+
---
+++ Assets/LEO/Scripts/Storage_Turret.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Storage_Turret : MonoBehaviour
+{
+    int resourceQuantity;
+
+
+
+
+
+
+
+    public void AddResource()
+    {
+        resourceQuantity++;
+    }
+    public void RemoveResource()
+    {
+        resourceQuantity--;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
